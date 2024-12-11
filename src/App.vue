37aa3b3c5ef751df37daa908bac57690a3ea9cc1
sayasha <2two2two8eight@mail.ru>
2024-12-11 14:09:48 +0300

    /

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <script setup>
 import Hero from './components/hero/Hero.vue'
 import Services from './components/services/Services.vue'
+import { onMounted } from 'vue'
+
+// onMounted(() => {
+//   gsap.registerPlugin(ScrollTriger, ScrollSmoother)
+//   ScrollSmoother.create({
+//     smooth: 1,
+//     effects: true,
+//   })
+// })
+
 </script>
 
 <template>
-    <Hero/>
-    <Services/>
+	<div class="wrapper">
+		<div class="content">
+			<Hero />
+			<Services />
+		</div>
+	</div>
 </template>
